85918452d84a3b0cdb090903e8617bc511ae0897
Soniya Faroda <143260442+soniyafaroda@users.noreply.github.com>
2025-12-06 13:24:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,21 +3,30 @@ const inputs = document.querySelectorAll(".code");
 inputs[0].focus();
 
 inputs.forEach((input, index) => {
+
   input.addEventListener("input", (e) => {
     const value = e.target.value;
-    if (value && index < inputs.length - 1) {
-      inputs[index + 1].focus(); // Move forward
+
+    // Accept only digits
+    if (!/^[0-9]$/.test(value)) {
+      input.value = "";
+      return;
+    }
+
+    // Move to next input
+    if (index < inputs.length - 1) {
+      inputs[index + 1].focus();
     }
   });
 
   input.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-      if (input.value === "" && index > 0) {
+      input.value = "";
+
+      if (index > 0) {
         inputs[index - 1].focus();
-        inputs[index - 1].value = ""; // Clear previous
-      } else {
-        input.value = "";
       }
     }
   });
+
 });
